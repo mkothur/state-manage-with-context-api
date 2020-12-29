223b056282a2fc67b5783adf361e7eb935fb6eaf
Mahesh Kothur <mkothur@us.ibm.com>
2020-12-29 10:13:50 -0500

    pagination with Todos array

--- src/MovieList.js
+++ src/MovieList.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import Movie from "./Movie";
 import FilterExample from "./FilterExample";
 import { MovieContext } from "./MovieContext";
+import TodoApp from "./TodoApp";
 
 const MovieList = () => {
   const [movies, setMovies] = useContext(MovieContext);
@@ -12,6 +13,7 @@ const MovieList = () => {
         <Movie name={movie.name} price={movie.price} key={movie.id} />
       ))}
       <FilterExample />
+      <TodoApp />
     </div>
   );
 };
